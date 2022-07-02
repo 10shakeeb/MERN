e131a49ac1b758c7970f32823d5d7e642fd9d9e0
10shakeeb <82671431+10shakeeb@users.noreply.github.com>
2022-07-02 15:37:18 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://shakeeb:shakeeb123@cluster0.pz2mc8z.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5001;
+const PORT = process.env.PORT|| 5002;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
