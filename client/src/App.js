0b20d2998f20d2d004cc4deb3b5af39247212613
10shakeeb <82671431+10shakeeb@users.noreply.github.com>
2022-07-13 19:47:53 +0100

    material ui core

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import bricks from './images/bricks.png';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+       <Container maxwidth='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align='center'>Bricks</Typography>
+                <img src = {bricks} alt='bricks' height='60'/>
+
+            </AppBar>
+
+       </Container>
     );
 }
 
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -12,6 +12,17 @@
       }
  }
 
- export const createPost = (req,res) => {
-   res.send('Post Creation');
+ export const createPost = async (req,res) => {
+   const post = req.body;
+
+   const newPost = new PostMessage(post);
+   try {
+      await newPost.save();
+
+      res.status(201).json(newPost);
+      
+   } catch (error) {
+      res.status(409).json({ message: error.message });
+      
+   }
  }
