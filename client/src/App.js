d17c6e8ebf17a0b7d34065fc4fefc015e27c8e79
10shakeeb <82671431+10shakeeb@users.noreply.github.com>
2022-07-14 16:59:54 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import bricks from './images/bricks.png';
+import useStyles from './styles'
 
 const App = () => {
+    const classes = useStyles();
     return (
        <Container maxwidth='lg'>
-            <AppBar position='static' color='inherit'>
-                <Typography variant='h2' align='center'>Bricks</Typography>
-                <img src = {bricks} alt='bricks' height='60'/>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>Bricks</Typography>
+                <img className={classes.image} src = {bricks} alt='bricks' height='60'/>
 
             </AppBar>
             <Grow in>
