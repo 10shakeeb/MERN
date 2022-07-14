7ec0af2532386e0f896411b3939d1c2b45bea27c
10shakeeb <82671431+10shakeeb@users.noreply.github.com>
2022-07-14 16:54:16 +0100

    material UI style

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 import bricks from './images/bricks.png';
 
 const App = () => {
@@ -15,9 +17,11 @@ const App = () => {
                 <Container>
                     <Grid container justify='space-between' alignItems='stretch' spacing={3}>
                         <Grid item xs={12} sm={7}>
+                            <Posts/>
 
                         </Grid>
                         <Grid item xs={12} sm={4}>
+                            <Form/>
 
                         </Grid>
 
