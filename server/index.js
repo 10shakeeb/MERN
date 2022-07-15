dba604750b6fd7c1d3aba9e18665575cd49d32af
10shakeeb <82671431+10shakeeb@users.noreply.github.com>
2022-07-15 20:33:14 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes)
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://shakeeb:shakeeb123@cluster0.pz2mc8z.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5002;
