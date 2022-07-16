576b48978be212b7d9109122f8ad22ea17f0bdfd
10shakeeb <82671431+10shakeeb@users.noreply.github.com>
2022-07-16 17:23:24 +0100

    structured props

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbAltUpIcon from '@material-ui/icons/ThumbAltUpIcon';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 import useStyles from './styles';
 
-const Post = () => {
+const Post = (post) => {
     const classes = useStyles();
     return(
-        <h1>POST</h1>
+        <Card className={ classes.card }>
+            <CardMedia className={classes.media} image = { post.selectedFile } title = { post.title } />
+
+        </Card>
     );
 }
 
