872144961b7842f21b9a82bb7497071f6990ed2b
10shakeeb <82671431+10shakeeb@users.noreply.github.com>
2022-07-28 19:18:39 +0100

    fixed the delete button

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -47,7 +47,20 @@ export const deletePost = (id) => async (dispatch) => {
     try {
         await api.deletePost(id);
 
-        dispatch({type: 'Delete', payload: id});
+        dispatch({type: 'DELETE', payload: id});
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+
+        dispatch({ type: 'UPDATE', payload: data});
+
         
     } catch (error) {
         console.log(error);
