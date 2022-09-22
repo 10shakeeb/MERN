b0ae5f823bb21d6ca9737503907260184149d764
10shakeeb <82671431+10shakeeb@users.noreply.github.com>
2022-09-22 18:59:51 +0100

    fixed routes to change between auth and home

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Container } from '@material-ui/core';
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
  
@@ -15,7 +16,6 @@ const App = () => (
                 <Route path='/' element={<Home/>} />
                 <Route path='auth' element={<Auth/>} />
             </Routes>
-            <Home />
         </Container>
     </BrowserRouter>
 );
