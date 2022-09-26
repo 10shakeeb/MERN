87ab680f4067fb77fadc0bfee5f79d8e1976bb2f
Shakeeb Jumaan <shakeeb.jumaan@kcl.ac.uk>
2022-09-26 22:11:29 +0100

    add switchMode function to allow change in form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -10,15 +10,22 @@ const Auth = () => {
   const classes = useStyles();
   const [showPassword, setShowPassword] = useState(false);
 
-  const isSignup = false;
+  const [isSignup, setIsSignup] = useState(false);
 
-  const handleShowPassword = () => setShowPassword
+  const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword)
 
   const handleSubmit = () => {
 
   };
 
   const handleChange = () => {
+
+
+  };
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup );
+    handleShowPassword(false);
 
   };
 
@@ -40,6 +47,19 @@ const Auth = () => {
                     )}
                     <Input name='email' label='Email Address' handleChange={handleChange} type='email' />
                     <Input name='password' label='Password' handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
+                    {isSignup && <Input name='confirmPassword' label='Repeat Password' handleChange={handleChange} type='password' />}
+
+                </Grid>
+                <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+                    {isSignup ? 'Sign Up' : 'Sign In'}
+                </Button>
+                <Grid type='container' justify='flex-end'>
+                    <Grid item>
+                        <Button onClick={switchMode}>
+                            { isSignup ? 'Already have an account? Sign In' : "Don't have an account? Sign up?"}
+
+                        </Button>
+                    </Grid>
                 </Grid>
             </form>
         </Paper>
