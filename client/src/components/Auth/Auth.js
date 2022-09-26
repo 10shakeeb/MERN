3d1fce6452339d3f5215a71f28a90bf7d1e7f129
Shakeeb Jumaan <shakeeb.jumaan@kcl.ac.uk>
2022-09-26 21:51:18 +0100

    add handleShowpassword function

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
@@ -8,8 +8,11 @@ import Input from './Input';
 
 const Auth = () => {
   const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
 
   const isSignup = false;
+
+  const handleShowPassword = () => setShowPassword
 
   const handleSubmit = () => {
 
@@ -29,21 +32,19 @@ const Auth = () => {
             <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
             <form className={classes.form} onSubmit={handleSubmit}>
                 <Grid container spacing={2}>
-                    {
-                        isSignup && (
-                            <>
-                                <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus xs={6}/>
-                                
-                                
-                                <Input name='lastName' label='Last Name' handleChange={handleChange} autoFocus xs={6}/>
-                            </>
-                        )
-                    }
+                    { isSignup && (
+                    <>
+                        <Input name='firstName' label='First Name' handleChange={handleChange} half/>
+                        <Input name='lastName' label='Last Name' handleChange={handleChange} half/>
+                    </>
+                    )}
+                    <Input name='email' label='Email Address' handleChange={handleChange} type='email' />
+                    <Input name='password' label='Password' handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
                 </Grid>
             </form>
         </Paper>
     </Container>
   );
-}
+};
 
 export default Auth;
